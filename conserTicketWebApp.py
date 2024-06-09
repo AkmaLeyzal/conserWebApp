a825d57b94f5ae021c3960bb3f4b724ae1e287e5
Maleyzal <155442269+AkmaLeyzal@users.noreply.github.com>
2024-06-09 15:22:49 +0700

    Update conserTicketWebApp.py

--- conserTicketWebApp.py
+++ conserTicketWebApp.py
@@ -235,7 +235,8 @@ def main_menu():
     elif st.session_state.page == "Lacak Antrian Tiket":
         st.header("Lacak Antrian Tiket")
         column1, column2 = st.columns([8, 2])
-        name_to_search = column1.text_input("Cari berdasarkan nama")
+        st.write("Cari berdasarkan nama")
+        name_to_search = column1.text_input("")
         if column2.button("Cari", key="search_button"):
             payment_queue = st.session_state['payment_queue']
             found_tickets = payment_queue.search_by_name(name_to_search)
