6dc9eb2e2673b9e79b77db6fb2833eae8174e8ec
Maleyzal <155442269+AkmaLeyzal@users.noreply.github.com>
2024-06-12 18:57:47 +0700

    Update conserTicketWebApp.py

--- conserTicketWebApp.py
+++ conserTicketWebApp.py
@@ -317,6 +317,7 @@ def main_menu():
                         col4.write(ticket['category'])
                         col5.write(ticket['quantity'])
                         col6.write(ticket['total_price'])
+                        st.write("---")
             else:
                 st.warning(f"Tidak ada antrian")
 
